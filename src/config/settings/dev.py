9df1c12dd573b594f8e634620e6d3c9fd0d430b2
user <lynnyk.first@gmail.com>
2024-10-16 20:14:55 +0300

    2

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,4 +1,4 @@
-from config.settings.base import *  # NOQA
+from config.settings.base import INSTALLED_APPS, MIDDLEWARE, BASE_DIR  # NOQA
 
 SECRET_KEY = "django-insecure-^at&*_lw2l40k&syn3wy2!i7o*jko_yp3i@(vx3(h%kiteb9tc"
 
